f2b4839b6fcfe2e0d710dd309fbdf06a24543d00
joanvelja <joan.velja@studbocconi.it>
2024-05-01 17:55:26 +0200

    run_qa

--- util/mask_ops.py
+++ util/mask_ops.py
@@ -26,15 +26,22 @@ def restore_tensors_by_mask(
     keep_tensors: Optional[torch.FloatTensor] = None, 
     skip_tensors: Optional[torch.FloatTensor] = None, 
     ids_restore: Optional[torch.LongTensor] = None,
-):  
-    # when using this function with skip_mask for early-exit
+):
+    # Ensure keep_tensors and skip_tensors are reshaped if they have a dimension of zero
     if not len(keep_tensors.shape):
         keep_tensors = keep_tensors.reshape(-1,)
     if not len(skip_tensors.shape):
         skip_tensors = skip_tensors.reshape(-1,)
+    
+    # Concatenate keep_tensors and skip_tensors along the 0th dimension
     tensors_ = torch.cat([keep_tensors, skip_tensors], dim=0)
     t_shape = tensors_.shape
 
+    # Ensure ids_restore is a LongTensor
+    if not isinstance(ids_restore, torch.LongTensor):
+        ids_restore = ids_restore.long()  # Convert ids_restore to LongTensor
+
+    # Apply torch.gather based on the shape of tensors_
     if len(t_shape) == 1:
         tensors = torch.gather(tensors_, 0, index=ids_restore)
     elif len(t_shape) == 2:
@@ -44,4 +51,4 @@ def restore_tensors_by_mask(
     elif len(t_shape) == 4:
         tensors = torch.gather(tensors_, 0, index=ids_restore.reshape(-1, 1, 1, 1).repeat(1, t_shape[-3], t_shape[-2], t_shape[-1]))
 
-    return tensors    
+    return tensors
